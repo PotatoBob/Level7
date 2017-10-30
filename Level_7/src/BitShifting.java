27c2ec1e4c893476415126b629155488fa27e1da
Love4653 <Kaiya.park@gmail.com>
2017-10-29 19:01:34 -0700

    Made a BitShifting program

---
+++ Level_7/src/BitShifting.java
@@ -0,0 +1,16 @@
+
+public class BitShifting {
+	public static void main(String[] args) {
+		int x = 1;
+		x = x << 1;
+		System.out.println(x);
+		
+		x = 3;
+		x = x << 3;
+		System.out.println(x);
+		
+		x = 5;
+		x = x << 2;
+		System.out.println(x);
+	}
+}
